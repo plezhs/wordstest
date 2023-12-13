00dbea634fa895ce55bf81a1ef8cefa6321684d9
plezhs <pramasns@gmail.com>
2023-12-13 15:55:01 +0900

    main

--- wordtest/gui.py
+++ wordtest/gui.py
@@ -1,1 +1,20 @@
-from PyQt5 import *
+import typing
+from PyQt5 import QtCore
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtWidgets import QWidget
+
+class Main(QDialog):
+    def __init__(self):
+        QDialog.__init__(self)
+
+        edit = QLineEdit()
+
+        layout = QVBoxLayout()
+        layout.addWidget(edit)
+
+        self.setLayout(layout)
+app = QApplication([])
+dialog = Main()
+dialog.show()
+app.exec_()
--- wordtest/main.py
+++ wordtest/main.py
@@ -15,29 +15,28 @@ words=[]
 answer = []
 num=0
 
-with open(resource_path('resource\\words.txt'),'r', encoding='UTF-8') as a:
-    word = a.read().splitlines()
-    for i in word:
-        words.append(i)
-with open(resource_path('resource\\wordsmean.txt'),'r', encoding='UTF-8') as b:
-    mean = b.read().splitlines()
-    for j in mean:
-        answer.append(j)
-question = dict(zip(words,answer))
+# with open(resource_path('resource\\words.txt'),'r', encoding='UTF-8') as a:
+#     word = a.read().splitlines()
+#     for i in word:
+#         words.append(i)
+# with open(resource_path('resource\\wordsmean.txt'),'r', encoding='UTF-8') as b:
+#     mean = b.read().splitlines()
+#     for j in mean:
+# #         answer.append(j)
+# question = dict(zip(words,answer))
 unitnum = 1
-with open(resource_path(f'unit\\{unitnum}.json'),'w',encoding='UTF-8') as jsonfile:
-    json.dump(question,jsonfile,indent=2,ensure_ascii=False)
+# with open(resource_path(f'unit\\{unitnum}.json'),'w',encoding='UTF-8') as jsonfile:
+#     json.dump(question,jsonfile,indent=2,ensure_ascii=False)
 with open(resource_path(f'unit\\{unitnum}.json'),'r',encoding='UTF-8') as jsondict:
-    testdict = json.load(jsondict)
-a = list(testdict.keys())
-print (a)
-# random.shuffle(words)
-# for l in range(1,len(words)+1):
-#     uanswer = input(words[num]+'\n')
-#     ranswer = str(question[words[num]])
-#     if uanswer == ranswer:
-#         print('O')
-#     else:
-#         print('X')
-#         print(ranswer)
-#     num+=1
+    question = json.load(jsondict)
+words = list(question.keys())
+random.shuffle(words)
+for l in range(1,len(words)+1):
+    uanswer = input(words[num]+'\n')
+    ranswer = str(question[words[num]])
+    if uanswer == ranswer:
+        print('O')
+    else:
+        print('X')
+        print(ranswer)
+    num+=1
